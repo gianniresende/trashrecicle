bc23c0068c9a2623254f4f9c86c84edc559120fd
gianniresende <gianniresende@gmail.com>
2020-06-04 17:48:53 -0300

    feat: adding controllers and respective routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,15 @@
 import express from 'express'
-import knex from './database/connection'
+import PointsController from './controllers/pointsController'
+import Itemscontroller from './controllers/itemsController'
 
 const routes = express.Router()
+const pointsController = new PointsController()
+const itemscontroller = new Itemscontroller()
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('items').select('*')
-  const serializedItems = items.map(item => {
-    return {
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`
-    }
-  })
-  return response.json({serializedItems})
-})
+routes.get('/items', itemscontroller.index)
+
+routes.post('/points', pointsController.create)
+routes.get('/points/:id', pointsController.show)
+routes.get('/points', pointsController.index)
 
 export default routes
